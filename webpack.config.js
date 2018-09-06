59ba97755c7374ff07fe165cfa53d0c1ff8127df
ravinder <ravinderkumar1495@gmail.com>
2018-09-06 09:44:25 +0530

    portfolio website designed using bootstrap 4

--- webpack.config.js
+++ webpack.config.js
@@ -28,6 +28,15 @@ module.exports = {
         }, {
           loader: 'sass-loader' // compiles Sass to CSS
         }]
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {}
+          }
+        ]
       }
     ]
   },
